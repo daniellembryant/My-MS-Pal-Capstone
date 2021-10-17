501f3ddcd3135d3376e647459ca96f0683d16f8e
daniellembryant <danbry08@yahoo.com>
2021-10-17 13:18:39 -0500

    Adds authentication information and test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
 # User.create!([
 #   {name: "Alex Jay", email: "alex99@gmail.com", age_group: "18-35", password: "password", password_confirmation: "password", location: "Portland, OR", diagnosis_date: "1/23/2018", image_url: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"}, 
 #   {name: "Jackson Samuel", email: "sam_1999@yahoo.com", age_group: "36-50", password: "password", password_confirmation: "password", location: "Milwaukee, WI",diagnosis_date: "3/10/1988", image_url: "https://images.unsplash.com/photo-1484516396415-4971641de8e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2662&q=80"}, 
@@ -12,3 +15,15 @@
 #   {name: "John Bradford", email: "bradfordj@yahoo.com", age_group: "36-50", password: "password", password_confirmation: "password", location: "Atlanta, GA",diagnosis_date: "", image_url: "https://images.unsplash.com/photo-1521886243261-7fc82ca5cb60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1373&q=80"}, 
 #   {name: "Sophia Gia", email: "sg@gmail.com", age_group: "18-35", password: "password", password_confirmation: "password", location: "Phoenix, AZ", diagnosis_date: "2/28/2001", image_url: "https://images.unsplash.com/photo-1531986733711-de47444e8cd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"}, 
 # ])
+
+# Group.create!([{
+#   name: "MS Warriors", summary: "All welcome", location: "Miami, FL", image_url: ""
+# }])
+
+# UserGroup.create!([{
+#   user_id: 1, group_id: 1
+# }])
+
+# Message.create!([{
+#   user_id: 1, group_id: 1, body: "I'm so happy to be a part of this group!"
+# }])
