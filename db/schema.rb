a60a251f164a2d9660cabbe69bb2d1118fe165c1
daniellembryant <danbry08@yahoo.com>
2021-10-26 16:15:34 -0500

    removes password firld from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_222408) do
+ActiveRecord::Schema.define(version: 2021_10_26_210935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2021_10_22_222408) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
     t.boolean "admin", default: false
   end
 
